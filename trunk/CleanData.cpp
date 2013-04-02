1f59750f9cb5bfcbf494c1ba5e906b9961e6791a
lee.mephisto@gmail.com <lee.mephisto@gmail.com@df4cddc4-f451-2a10-c478-0fa1516a318c>
2013-04-02 01:57:52 +0000

    remove the redundances

--- trunk/CleanData.cpp
+++
@@ -1,5 +0,0 @@
-#!/home/rui/work/root/bin/root
-#Filename: CleanData.cpp
-{
-	gROOT->ProcessLine(".! rm -rf *.root *.txt *.pdf *.gate *.pdf");
-}
--- trunk/FloatCheck.cc
+++
@@ -1,4 +0,0 @@
-bool FloatCheck(int ch)
-{
-	return ((ch%32)&&((ch+1)%32))? 1: 0;
-}
