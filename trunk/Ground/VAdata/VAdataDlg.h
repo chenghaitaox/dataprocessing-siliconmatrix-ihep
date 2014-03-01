b823835a1c5a278cbbd4d5dc26f683c7a582cbaf
yuodaa@gmail.com <yuodaa@gmail.com@df4cddc4-f451-2a10-c478-0fa1516a318c>
2014-03-01 07:50:01 +0000

    同时显示DAQM,DVA,VA ID，地址范围

--- trunk/Ground/VAdata/VAdataDlg.h
+++ trunk/Ground/VAdata/VAdataDlg.h
@@ -31,16 +31,37 @@ protected:
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
 public:
-
+	//修改VA ID的一系列操作
 	int VAID;//VA的ID，从1~72
 	CEdit edit_vaid;//VA ID的编辑框
 	int GetVAID();//从VA ID的编辑框得到VA ID
 	void SetVAID(int);//赋值新的VA ID，刷写数据
-	afx_msg void EDIT_Change();//编辑框编辑
-	CSpinButtonCtrl ctrl_vaid;//VA ID的上下箭头
-	afx_msg void SPIN_VAID(NMHDR *pNMHDR, LRESULT *pResult);//点击上下箭头
+	afx_msg void EDIT_vaid();//编辑框编辑
+	CSpinButtonCtrl spin_vaid;//VA ID的上下箭头
+	afx_msg void SPIN_vaid(NMHDR *pNMHDR, LRESULT *pResult);//点击上下箭头
 
 
+	//修改DAQM的一系列操作
+	int DAQM;//VA的DAQM，从0-11
+	CEdit edit_daqm;
+	int GetDAQM();
+	void SetDAQM(int);
+	afx_msg void EDIT_daqm();
+	CSpinButtonCtrl spin_daqm;
+	afx_msg void SPIN_daqm(NMHDR *pNMHDR, LRESULT *pResult);
+
+
+	//修改DVA的一系列操作
+	int DVA;//VA的DVA，从0-5
+	CEdit edit_dva;
+	int GetDVA();
+	void SetDVA(int);
+	CSpinButtonCtrl spin_dva;
+	afx_msg void EDIT_dva();
+	afx_msg void SPIN_dva(NMHDR *pNMHDR, LRESULT *pResult);
+
+
+	//编辑框阵列的操作
 	afx_msg void BTN_NewVAData();//把新的VA数据赋值给buffer
 	CArray<CEdit*,CEdit*> v_edit; //编辑框阵列
 	char buffer[DET_NUM];//存储每个通道的数据
@@ -52,6 +73,9 @@ public:
 
 
 	afx_msg void BTN_Output();//打开输出文件
+
+
+	CStatic static_address;
 
 
 	void ResetAll();//清空
