f88e2693f1fd30fce9d6daa0f771179542db06a6
yuodaa <yuodaa@gmail.com>
2014-03-01 07:50:01 +0000

    同时显示DAQM,DVA,VA ID，地址范围

--- Ground/VAdata/VAdataDlg.cpp
+++ Ground/VAdata/VAdataDlg.cpp
@@ -14,20 +14,177 @@
 
 
 
+void CProjectDlg::ResetAll()
+{
+	good=0;
+	VAID=-1;DVA=-1;DAQM=-1;
+	for(int i=0;i<128;i++)
+	{
+		v_edit[i]->SetWindowText(CString(""));
+	}
+	edit_vaid.SetWindowText(CString("-1"));
+	edit_daqm.SetWindowText(CString("-1"));
+	edit_dva.SetWindowText(CString("-1"));
+
+	static_address.SetWindowText(CString(""));
+}
+int CProjectDlg::GetVAID()
+{
+	if(!good) return -1;//没有数据
+
+
+	int tempVAID;CString str;
+	edit_vaid.GetWindowText(str);
+	tempVAID=_ttoi(str);//从编辑框得到新的VA ID
+
+
+	if(tempVAID>72 || tempVAID<1)//越界检查
+	{
+		str.Format(__T("%d"),VAID);
+		edit_vaid.SetWindowText(str);//编辑框显示旧值
+		return VAID;//返回旧值
+	}
+	else
+	{
+		return tempVAID;//返回新值
+	}
+}
+int CProjectDlg::GetDAQM()
+{
+	if(!good) return -1;//没有数据
+
+
+	int tempDAQM;CString str;
+	edit_daqm.GetWindowText(str);
+	tempDAQM=_ttoi(str);//从编辑框得到新的VA ID
+
+
+	if(tempDAQM>11 || tempDAQM<0)//越界检查
+	{
+		str.Format(__T("%d"),DAQM);
+		edit_daqm.SetWindowText(str);//编辑框显示旧值
+		return DAQM;//返回旧值
+	}
+	else
+	{
+		return tempDAQM;//返回新值
+	}
+}
+int CProjectDlg::GetDVA()
+{
+	if(!good) return -1;//没有数据
+
+
+	int tempDVA;CString str;
+	edit_dva.GetWindowText(str);
+	tempDVA=_ttoi(str);//从编辑框得到新的VA ID
+
+
+	if(tempDVA>5 || tempDVA<0)//越界检查
+	{
+		str.Format(__T("%d"),DVA);
+		edit_dva.SetWindowText(str);//编辑框显示旧值
+		return DVA;//返回旧值
+	}
+	else
+	{
+		return tempDVA;//返回新值
+	}
+}
+void CProjectDlg::SetVAID(int tempVAID)
+{
+	if(!good) return;//没有数据
+
+
+	if(tempVAID>72 || tempVAID<1) return;//越界检查
+
+
+	if(tempVAID==VAID)return;//数据没有变化
+
+
+	VAID=tempVAID;//把新值赋给VA ID
+	DAQM=(VAID-1)/6;DVA=(VAID-1)%6;//计算DAQM和DVA
+	
+	
+	CString str;
+	//VAID编辑框显示新值
+	str.Format(__T("%d"),VAID);
+	edit_vaid.SetWindowText(str);
+	//DAQM编辑框显示新值
+	str.Format(__T("%d"),DAQM);
+	edit_daqm.SetWindowText(str);
+	//DVA编辑框显示新值
+	str.Format(__T("%d"),DVA);
+	edit_dva.SetWindowText(str);
+
+
+	for(int i=0;i<128;i++)//循环2个VA的数据，得到值
+	{
+		str.Format(__T("%d"),int(buffer[(VAID-1)*128+i]&0x00ff));//从buffer得到2个VA数据
+		v_edit[i]->SetWindowText(str);//在对话框中显示2个VA数据
+	}
+
+
+	str.Format(__T("地址范围(从1开始)：%d - %d"),(VAID-1)*128+1,VAID*128);
+	static_address.SetWindowText(str);
+}
+
+void CProjectDlg::SetDAQM(int tempDAQM)
+{
+	if(!good) return;//没有数据
+
+
+	if(tempDAQM>11 || tempDAQM<0) return;//越界检查
+
+
+	if(tempDAQM==DAQM)return;//数据没有变化
+
+
+	DAQM=tempDAQM;//把新值赋给VA ID
+	int tempVAID=DAQM*6+DVA+1;//计算VA ID
+	
+	
+	SetVAID(tempVAID);
+}
+
+void CProjectDlg::SetDVA(int tempDVA)
+{
+	if(!good) return;//没有数据
+
+
+	if(tempDVA>5 || tempDVA<0) return;//越界检查
+
+
+	if(tempDVA==DVA)return;//数据没有变化
+
+
+	DVA=tempDVA;//把新值赋给VA ID
+	int tempVAID=DAQM*6+DVA+1;//计算VA ID
+	
+	
+	SetVAID(tempVAID);
+}
 
 CProjectDlg::CProjectDlg(CWnd* pParent /*=NULL*/)
 	: CDialog(CProjectDlg::IDD, pParent)
 {
 	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
 }
-
 void CProjectDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
 
 
 	DDX_Control(pDX,IDC_EDIT1,edit_vaid);
-	DDX_Control(pDX,IDC_SPIN1,ctrl_vaid);
+	DDX_Control(pDX,IDC_SPIN1,spin_vaid);
+
+	DDX_Control(pDX,IDC_EDIT2,edit_daqm);
+	DDX_Control(pDX,IDC_SPIN2,spin_daqm);
+
+	DDX_Control(pDX,IDC_EDIT3,edit_dva);
+	DDX_Control(pDX,IDC_SPIN3,spin_dva);
+
+	DDX_Control(pDX,IDC_STATIC_ADDRESS,static_address);
 
 }
 
@@ -35,12 +192,16 @@ BEGIN_MESSAGE_MAP(CProjectDlg, CDialog)
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
 	//}}AFX_MSG_MAP
-	ON_NOTIFY(UDN_DELTAPOS, IDC_SPIN1, &CProjectDlg::SPIN_VAID)
+	ON_NOTIFY(UDN_DELTAPOS, IDC_SPIN1, &CProjectDlg::SPIN_vaid)
 	ON_BN_CLICKED(IDC_BUTTON2, &CProjectDlg::BTN_NewVAData)
 	ON_BN_CLICKED(IDC_BUTTON3, &CProjectDlg::BTN_OpenFile)
 	ON_BN_CLICKED(IDC_BUTTON4, &CProjectDlg::BTN_Output)
 	ON_BN_CLICKED(IDC_BUTTON5, &CProjectDlg::BTN_GenerateTest)
-	ON_EN_CHANGE(IDC_EDIT1, &CProjectDlg::EDIT_Change)
+	ON_EN_CHANGE(IDC_EDIT1, &CProjectDlg::EDIT_vaid)
+	ON_EN_CHANGE(IDC_EDIT2, &CProjectDlg::EDIT_daqm)
+	ON_EN_CHANGE(IDC_EDIT3, &CProjectDlg::EDIT_dva)
+	ON_NOTIFY(UDN_DELTAPOS, IDC_SPIN2, &CProjectDlg::SPIN_daqm)
+	ON_NOTIFY(UDN_DELTAPOS, IDC_SPIN3, &CProjectDlg::SPIN_dva)
 END_MESSAGE_MAP()
 
 
@@ -56,14 +217,13 @@ BOOL CProjectDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// 设置小图标
 
 
-
+	//画编辑框阵列
 	const int x_begin=240;
 	const int y_begin=20;
 	const int x_period=50;
 	const int y_period=20;
 	const int x_width=40;
 	const int y_width=17;
-
 	CEdit *pEdit;
 	int xx=x_begin,yy=y_begin;
 	for(int j=0;j<16;j++)//横行
@@ -74,7 +234,7 @@ BOOL CProjectDlg::OnInitDialog()
 		{
 			pEdit=new CEdit;
 			pEdit->Create(WS_CHILD | WS_VISIBLE | WS_TABSTOP | WS_BORDER | ES_CENTER,
-				CRect(xx,yy,xx+x_width,yy+y_width),this,0);//画编辑框阵列
+				CRect(xx,yy,xx+x_width,yy+y_width),this,0);
 			v_edit.Add(pEdit);
 
 			xx+=x_period;
@@ -84,9 +244,7 @@ BOOL CProjectDlg::OnInitDialog()
 	}
 
 
-	good=0;//初始为没有文件
-	VAID=-1;//初始VA ID为-1
-	edit_vaid.SetWindowText(CString("-1"));//初始VA ID显示为-1
+	ResetAll();//重置所有
 
 
 	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
@@ -128,74 +286,6 @@ HCURSOR CProjectDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
-int CProjectDlg::GetVAID()
-{
-	if(!good) return -1;//没有数据
-
-
-	int tempVAID;CString str;
-	edit_vaid.GetWindowText(str);
-	tempVAID=_ttoi(str);//从编辑框得到新的VA ID
-
-
-	if(tempVAID>72 || tempVAID<1)//越界检查
-	{
-		str.Format(__T("%d"),VAID);
-		edit_vaid.SetWindowText(str);//编辑框显示旧值
-		return VAID;//返回旧值
-	}
-	else
-	{
-		return tempVAID;//返回新值
-	}
-}
-void CProjectDlg::SetVAID(int tempVAID)
-{
-	if(!good) return;//没有数据
-
-
-	if(tempVAID>72 || tempVAID<1) return;//越界检查
-
-
-	if(tempVAID==VAID)return;//数据没有变化
-
-
-	VAID=tempVAID;//把新值赋给VA ID
-	
-	
-	CString str;
-	str.Format(__T("%d"),VAID);
-	edit_vaid.SetWindowText(str);//编辑框显示新值
-
-
-	for(int i=0;i<128;i++)//循环2个VA的数据，得到值
-	{
-		str.Format(__T("%d"),int(buffer[(VAID-1)*128+i]&0x00ff));//从buffer得到2个VA数据
-		v_edit[i]->SetWindowText(str);//在对话框中显示2个VA数据
-	}
-}
-
-void CProjectDlg::ResetAll()
-{
-}
-void CProjectDlg::SPIN_VAID(NMHDR *pNMHDR, LRESULT *pResult)
-{
-	LPNMUPDOWN pNMUpDown = reinterpret_cast<LPNMUPDOWN>(pNMHDR);
-	
-	if(!good) return;
-	if(pNMUpDown->iDelta==1)
-	{
-		SetVAID(VAID-1);
-	}
-	else
-	{
-		SetVAID(VAID+1);
-	}
-
-
-	*pResult = 0;
-}
-
 void CProjectDlg::BTN_NewVAData()
 {
 	if(!good) return;//无数据打开
@@ -217,6 +307,9 @@ void CProjectDlg::BTN_NewVAData()
 
 void CProjectDlg::BTN_OpenFile()
 {
+	ResetAll();//首先清空
+
+
 	CFileDialog mydlg(TRUE);//打开文件对话框
 	if(mydlg.DoModal()!=IDOK) return;//没有选择文件
 
@@ -231,7 +324,7 @@ void CProjectDlg::BTN_OpenFile()
 
 
 	good=1;
-	SetVAID(1);
+	SetVAID(1);//显示数据
 }
 
 void CProjectDlg::BTN_Output()
@@ -253,6 +346,9 @@ void CProjectDlg::BTN_Output()
 
 void CProjectDlg::BTN_GenerateTest()
 {
+	ResetAll();//首先清空
+
+
 	for(int i=0;i<72;i++)
 	{
 		for(int j=0;j<128;j++)
@@ -266,7 +362,69 @@ void CProjectDlg::BTN_GenerateTest()
 	SetVAID(1);
 }
 
-void CProjectDlg::EDIT_Change()
+void CProjectDlg::EDIT_vaid()
 {
 	SetVAID(GetVAID());
+}
+
+void CProjectDlg::EDIT_daqm()
+{
+	SetDAQM(GetDAQM());
+}
+
+void CProjectDlg::EDIT_dva()
+{
+	SetDVA(GetDVA());
+}
+void CProjectDlg::SPIN_vaid(NMHDR *pNMHDR, LRESULT *pResult)
+{
+	LPNMUPDOWN pNMUpDown = reinterpret_cast<LPNMUPDOWN>(pNMHDR);
+	
+	if(!good) return;
+	if(pNMUpDown->iDelta==1)
+	{
+		SetVAID(VAID-1);
+	}
+	else
+	{
+		SetVAID(VAID+1);
+	}
+
+
+	*pResult = 0;
+}
+
+
+void CProjectDlg::SPIN_daqm(NMHDR *pNMHDR, LRESULT *pResult)
+{
+	LPNMUPDOWN pNMUpDown = reinterpret_cast<LPNMUPDOWN>(pNMHDR);
+
+	if(!good) return;
+	if(pNMUpDown->iDelta==1)
+	{
+		SetDAQM(DAQM-1);
+	}
+	else
+	{
+		SetDAQM(DAQM+1);
+	}
+
+	*pResult = 0;
+}
+
+void CProjectDlg::SPIN_dva(NMHDR *pNMHDR, LRESULT *pResult)
+{
+	LPNMUPDOWN pNMUpDown = reinterpret_cast<LPNMUPDOWN>(pNMHDR);
+
+	if(!good) return;
+	if(pNMUpDown->iDelta==1)
+	{
+		SetDVA(DVA-1);
+	}
+	else
+	{
+		SetDVA(DVA+1);
+	}
+
+	*pResult = 0;
 }
--- Ground/VAdata/resource.h
+++ Ground/VAdata/resource.h
@@ -6,11 +6,15 @@
 #define IDR_MAINFRAME                   128
 #define IDC_EDIT1                       1000
 #define IDC_SPIN1                       1001
-#define IDC_BUTTON1                     1002
 #define IDC_BUTTON2                     1003
 #define IDC_BUTTON3                     1004
 #define IDC_BUTTON4                     1005
 #define IDC_BUTTON5                     1006
+#define IDC_EDIT2                       1007
+#define IDC_EDIT3                       1008
+#define IDC_SPIN2                       1009
+#define IDC_SPIN3                       1010
+#define IDC_STATIC_ADDRESS              1011
 
 // Next default values for new objects
 // 
@@ -18,7 +22,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
